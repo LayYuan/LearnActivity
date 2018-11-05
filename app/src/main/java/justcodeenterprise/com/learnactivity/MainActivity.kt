0b4b86034d93a94c1adbe5240726bb0e1898333b
Yuan25 <chuahlayyuan@gmail.com>
2018-11-05 15:49:21 +0800

    Registering Broadcast Receivers

--- app/src/main/java/justcodeenterprise/com/learnactivity/MainActivity.kt
+++ app/src/main/java/justcodeenterprise/com/learnactivity/MainActivity.kt
@@ -1,13 +1,19 @@
 package justcodeenterprise.com.learnactivity
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +22,18 @@ class MainActivity : AppCompatActivity() {
     private val adapter by lazy { makeAdapter(taskList) }
 
     private val ADD_TASK_REQUEST = 1
+
+    private val tickReceiver by lazy { makeBroadcastReceiver() }
+
+    companion object {
+        private const val LOG_TAG = "MainActivityLog"
+
+        private fun getCurrentTimeStamp(): String {
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US)
+            val now = Date()
+            return simpleDateFormat.format(now)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -35,7 +53,29 @@ class MainActivity : AppCompatActivity() {
         taskListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id -> }
     }
 
+    override fun onResume() {
+        // 1
+        super.onResume()
+        // 2 You update the date and time TextView with the current time stamp, because the broadcast receiver is not currently registered.
+        dateTimeTextView.text = getCurrentTimeStamp()
+        // 3 You then register the broadcast receiver in onResume(). This ensures it will receive the broadcasts for ACTION_TIME_TICK.
+        // These are sent every minute after the time changes.
+        registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
+    }
 
+    override fun onPause() {
+        // 4
+        super.onPause()
+        // 5
+        try {
+            unregisterReceiver(tickReceiver)
+        } catch (e: IllegalArgumentException) {
+            Log.e(MainActivity.LOG_TAG, "Time tick Receiver not registered", e)
+        }
+    }
+
+
+    //after second task finish, retrieve data pass from second task
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         // 1 You check the requestCode to ensure the activity result is indeed for your add task request you started with
         // TaskDescriptionActivity.
@@ -70,4 +110,17 @@ class MainActivity : AppCompatActivity() {
     // for a single-expression function.
     private fun makeAdapter(list: List<String>): ArrayAdapter<String> =
         ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+
+    //Here, you create a BroadcastReceiver that sets the date and time on the screen if it receives a time change broadcast
+    // from the system.
+    private fun makeBroadcastReceiver(): BroadcastReceiver {
+        return object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_TIME_TICK) {
+                    dateTimeTextView.text = getCurrentTimeStamp()
+                }
+            }
+        }
+    }
 }
